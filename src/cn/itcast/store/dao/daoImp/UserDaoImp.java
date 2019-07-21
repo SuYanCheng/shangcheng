b288571babb7c36252cb33494f5a517ffe02a404
SuYancheng <15861931745@163.com>
2019-07-22 01:23:29 +0800

    first commit

---
+++ src/cn/itcast/store/dao/daoImp/UserDaoImp.java
@@ -0,0 +1,7 @@
+package cn.itcast.store.dao.daoImp;
+
+import cn.itcast.store.dao.UserDao;
+
+public class UserDaoImp implements UserDao {
+
+}
---
+++ src/cn/itcast/store/service/serviceImp/UserSerciceImp.java
@@ -0,0 +1,7 @@
+package cn.itcast.store.service.serviceImp;
+
+import cn.itcast.store.service.UserService;
+
+public class UserSerciceImp implements UserService {
+
+}
